14cee08dd9d9f773164de9727402aa6782f487e4
Miroslav Hnat <miroslav.hnat@adastragrp.com>
2019-08-20 12:35:54 +0200

    add LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+ //
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Miroslav Hnát on 20/08/2019.
+//  Copyright © 2019 Miroslav Hnát. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+ }
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Miroslav Hnát on 11/08/2019.
+//  Copyright © 2019 Miroslav Hnát. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
